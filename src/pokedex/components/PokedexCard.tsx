04b3cd76aa326ac0c8be7b2f43510410104cb1bd
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-01-24 09:40:34 -0300

    promises and update of pokedex details

--- src/pokedex/components/PokedexCard.tsx
+++ src/pokedex/components/PokedexCard.tsx
@@ -1,28 +1,65 @@
 import React from "react";
-import styled from "styled-components";
 import { PokemonListInterface } from "../../pokemon/services/listPokemons";
 import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { PokemonDetail } from "../../pokemon/interfaces/PokemonDetail";
+import PokemonDetails from "../../pokemon/services/PokemonDetails";
+import { Chip } from "@mui/material";
+import { Box } from "@mui/system";
 
 interface PokedexCardProps {
-  pokemon: PokemonListInterface;
+  pokemon: PokemonDetail;
 }
 
-const Card = styled.section`
-  padding: 4em;
-  border-radius: 0.5em;
-  background: papayawhip;
-`;
+// const Card = styled.section`
+//   padding: 4em;
+//   border-radius: 0.5em;
+//   background: papayawhip;
+// `;
 
 const PokedexCard: React.FC<PokedexCardProps> = ({ pokemon }) => {
   const navigate = useNavigate();
 
-  function handleClick(pokemon: PokemonListInterface) {
+  function handleClick(pokemon: PokemonDetail) {
     navigate(`/pokemon/${pokemon.name}`);
   }
 
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <>
-      <Card onClick={() => handleClick(pokemon)}>{pokemon.name}</Card>
+      <Card sx={{ maxWidth: 345 }} onClick={() => handleClick(pokemon)}>
+        <CardHeader
+          title={pokemon.name}
+          subheader={pokemon.types.map((type) => (
+              <Chip label={type.type.name} variant="outlined" />
+          ))}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={pokemon.sprites.front_default}
+          alt="Paella dish"
+        />
+      </Card>
     </>
   );
 };
--- src/pokemon/services/listPokemons.ts
+++ src/pokemon/services/listPokemons.ts
@@ -1,22 +1,32 @@
 import axios from "axios";
+import { PokemonDetail } from "../interfaces/PokemonDetail";
+import { getPokemonsDetails } from "./getPokemonDetails";
 
 export interface PokemonListInterface {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
-interface listPokemonsInterface{
-    count: number;
-    next: null | string;
-    previous: null | string;
-    results: PokemonListInterface[];
+interface listPokemonsInterface {
+  count: number;
+  next: null | string;
+  previous: null | string;
+  results: PokemonDetail[];
 }
 
-export async function listPokemons(): Promise<listPokemonsInterface>{
+export async function listPokemons(): Promise<listPokemonsInterface> {
+  const endpoint = `${process.env.REACT_APP_POKEAPI}/pokemon`;
 
-    const endpoint = `${process.env.REACT_APP_POKEAPI}/pokemon`
-   
-    const response = await axios.get<listPokemonsInterface>(endpoint);
+  const response = await axios.get<listPokemonsInterface>(endpoint);
 
-    return response.data;
+  const promiseArr = await response.data.results.map(({ name }) =>
+    getPokemonsDetails(name)
+  )
+
+  const resultsPromise = await Promise.all(promiseArr)
+
+  return {
+    ...response.data,
+    results: resultsPromise
+  };
 }
